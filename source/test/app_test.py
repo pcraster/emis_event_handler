b700f7cb4a7adced897cc2c40a3481becf11cbac
Kor de Jong <k.dejong1@uu.nl>
2017-03-22 11:36:53 +0100

    Update test code

--- source/test/app_test.py
+++ source/test/app_test.py
@@ -5,7 +5,8 @@ from emis_event_handler import create_app
 class AppTest(unittest.TestCase):
 
     def setUp(self):
-        self.app = create_app("testing")
+        self.app = create_app("test")
+        self.app.config["TESTING"] = True
 
 
     def tearDown(self):
